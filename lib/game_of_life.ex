44c8aaab933f856e7de9b0b09eca1801f78d9765
Daniel Errante <danoph@gmail.com>
2018-05-11 14:01:45 -0500

    adding test for converting strings into structs

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -1,4 +1,6 @@
-defmodule GameOfLife do
+defmodule GameOfLife.Game do
+  alias GameOfLife.Cell
+
   def tick(grid, 1) do
     #grid
     #|> Enum.with_index
@@ -9,6 +11,21 @@ defmodule GameOfLife do
         #IO.puts("#{y} => #{row}")
       #end)
     #end)
+  end
+
+  def to_grid(grid) do
+    grid
+    |> Enum.with_index
+    |> Enum.map(fn({row, y}) ->
+      row
+      |> Enum.with_index
+      |> Enum.map(fn
+        ({value, x }) -> %{ x: x, y: y, alive: value }
+        #({1, x }) -> cell = %{ x: x, y: y, alive: true }
+        #%Cell{} = { x, y, alive: false }
+        #IO.puts("#{cell.x},#{cell.y} => #{cell.alive}")
+      end)
+    end)
   end
 
   def print(grid) do
@@ -22,4 +39,8 @@ defmodule GameOfLife do
       end)
     end)
   end
+end
+
+defmodule GameOfLife.Cell do
+  defstruct x: nil, y: nil, alive: false
 end
--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -1,44 +1,38 @@
 defmodule GameOfLifeTest do
   use ExUnit.Case
-  doctest GameOfLife
+
+  alias GameOfLife.Game
+
+  @blinker [
+    [0,0,0,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+    [0,0,0,0,0],
+  ]
 
   #test "greets the world" do
-    #grid = [
-      #[0,0,0,0,0],
-      #[0,0,1,0,0],
-      #[0,0,1,0,0],
-      #[0,0,1,0,0],
-      #[0,0,0,0,0],
-    #]
-
-    #expected_grid = [
-      #[0,0,0,0,0],
-      #[0,0,0,0,0],
-      #[0,1,1,1,0],
-      #[0,0,0,0,0],
-      #[0,0,0,0,0],
-    #]
-
-    #assert GameOfLife.tick(grid, 1) == expected_grid
+    #assert GameOfLife.tick(@blinker, 1) == expected_grid
   #end
 
-  test "prints grid" do
-    grid = [
-      [0,0,0,0,0],
-      [0,0,0,0,0],
-      [0,1,1,1,0],
-      [0,0,0,0,0],
-      [0,0,0,0,0],
-    ]
+  #test "prints grid" do
+    ##expected_output = @blinker
+                      ##|> Enum.reduce(fn(row) ->
+                        ##row.reduce(fn(cell, acc) ->
+                        ##end)
+                      ##end)
+    #assert Game.print(@blinker) == @blinker
+  #end
 
+  test "to grid with cell structs" do
     expected_grid = [
-      [0,0,0,0,0],
-      [0,0,0,0,0],
-      [0,1,1,1,0],
-      [0,0,0,0,0],
-      [0,0,0,0,0],
+      [%{alive: 0, x: 0, y: 0}, %{alive: 0, x: 1, y: 0}, %{alive: 0, x: 2, y: 0}, %{alive: 0, x: 3, y: 0}, %{alive: 0, x: 4, y: 0}],
+      [%{alive: 0, x: 0, y: 1}, %{alive: 0, x: 1, y: 1}, %{alive: 1, x: 2, y: 1}, %{alive: 0, x: 3, y: 1}, %{alive: 0, x: 4, y: 1}],
+      [%{alive: 0, x: 0, y: 2}, %{alive: 0, x: 1, y: 2}, %{alive: 1, x: 2, y: 2}, %{alive: 0, x: 3, y: 2}, %{alive: 0, x: 4, y: 2}],
+      [%{alive: 0, x: 0, y: 3}, %{alive: 0, x: 1, y: 3}, %{alive: 1, x: 2, y: 3}, %{alive: 0, x: 3, y: 3}, %{alive: 0, x: 4, y: 3}],
+      [%{alive: 0, x: 0, y: 4}, %{alive: 0, x: 1, y: 4}, %{alive: 0, x: 2, y: 4}, %{alive: 0, x: 3, y: 4}, %{alive: 0, x: 4, y: 4}]
     ]
 
-    assert GameOfLife.print(grid) == expected_grid
+    assert Game.to_grid(@blinker) == expected_grid
   end
 end
